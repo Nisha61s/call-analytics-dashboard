cce1363b2aced34391261b0f89ec9892d4c7b0ec
Nisha <nishasankaran2006@gmail.com>
2026-07-30 14:57:29 +0100

    Fix Express 5 CORS options route

--- admin/server.js
+++ admin/server.js
@@ -69,7 +69,7 @@ connectDB().catch(() => {
 app.set("trust proxy", 1);
 app.use(helmet());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
